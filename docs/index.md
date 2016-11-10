b6252939bd00590a134edf55051c4fcd008c8abd
Alexander Belopolsky <a@enlnt.com>
2016-11-09 21:00:45 -0500

    Initial commit.
    Generated by Cookiecutter Pytest Plugin.
    
    See <https://github.com/pytest-dev/cookiecutter-pytest-plugin>.

---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Welcome to pytest-leaks
+
+A pytest plugin to trace resource leaks.
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import codecs
+from setuptools import setup
+
+
+def read(fname):
+    file_path = os.path.join(os.path.dirname(__file__), fname)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+setup(
+    name='pytest-leaks',
+    version='0.1.0',
+    author='Alexander Belopolsky',
+    author_email='alexander.belopolsky@gmail.com',
+    maintainer='Alexander Belopolsky',
+    maintainer_email='alexander.belopolsky@gmail.com',
+    license='MIT',
+    url='https://github.com/abalkin/pytest-leaks',
+    description='A pytest plugin to trace resource leaks.',
+    long_description=read('README.rst'),
+    py_modules=['pytest_leaks'],
+    install_requires=['pytest>=2.9.2'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+    ],
+    entry_points={
+        'pytest11': [
+            'leaks = pytest_leaks',
+        ],
+    },
+)
