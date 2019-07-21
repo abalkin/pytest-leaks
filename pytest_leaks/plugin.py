a9ffcba5835801b1865e9a50d43648c4f63a35ef
Pauli Virtanen <pav@iki.fi>
2019-07-21 21:37:54 +0300

    Use the patched refleak_38.py

--- pytest_leaks/plugin.py
+++ pytest_leaks/plugin.py
@@ -4,13 +4,17 @@ A pytest plugin to trace resource leaks
 """
 from __future__ import print_function
 
-import gc
 import sys
-import warnings
-from collections import OrderedDict as odict
-from inspect import isabstract
+from collections import OrderedDict
 
 import pytest
+
+
+from . import refleak_38 as refleak
+
+
+class Leaks(OrderedDict):
+    pass
 
 
 def pytest_addoption(parser):
@@ -40,7 +44,7 @@ reports are written to.  These parameters all have defaults (5, 4 and
 def pytest_configure(config):
     leaks = config.getvalue("leaks")
     if leaks:
-        if not hasattr(sys, 'gettotalrefcount') or True:
+        if not hasattr(sys, 'gettotalrefcount'):
             raise pytest.UsageError("pytest-leaks: tracking reference leaks requires "
                                     "running on a debug build of Python")
 
@@ -51,10 +55,6 @@ def pytest_configure(config):
 @pytest.fixture
 def leaks_checker(request):
     return request.config.pluginmanager.get_plugin('leaks_checker')
-
-
-class Leaks(odict):
-    pass
 
 
 class LeakChecker(object):
@@ -125,145 +125,15 @@ class LeakChecker(object):
         if leaked:
             tr.write_sep("=", 'leaks summary', cyan=True)
             for rep in leaked:
-                tr.line("%s: %r" % (rep.nodeid, self.leaks[rep.nodeid]))
+                tr.line("%s: %r" % (rep.nodeid, Leaks(self.leaks[rep.nodeid])))
+
+
+class Namespace(object):
+    pass
 
 
 def hunt_leaks(func, nwarmup, ntracked):
-    """Run a func multiple times, looking for leaks."""
-    # This code is hackish and inelegant, but it seems to do the job.
-    import copyreg
-    import collections.abc
-
-    # Save current values for cleanup() to restore.
-    fs = warnings.filters[:]
-    ps = copyreg.dispatch_table.copy()
-    pic = sys.path_importer_cache.copy()
-    try:
-        import zipimport
-    except ImportError:
-        zdc = None  # Run unmodified on platforms without zipimport support
-    else:
-        zdc = zipimport._zip_directory_cache.copy()
-    abcs = {}
-    for abc in [getattr(collections.abc, a) for a in collections.abc.__all__]:
-        if not isabstract(abc):
-            continue
-        for obj in abc.__subclasses__() + [abc]:
-            abcs[obj] = obj._abc_registry.copy()
-    repcount = nwarmup + ntracked
-    rc_deltas = [0] * repcount
-    alloc_deltas = [0] * repcount
-    # initialize variables to make pyflakes quiet
-    rc_before = alloc_before = 0
-    for i in range(repcount):
-        func()
-        alloc_after, rc_after = cleanup(fs, ps, pic, zdc, abcs)
-        if i >= nwarmup:
-            rc_deltas[i] = rc_after - rc_before
-            alloc_deltas[i] = alloc_after - alloc_before
-        alloc_before = alloc_after
-        rc_before = rc_after
-
-    # These checkers return False on success, True on failure
-    def check_rc_deltas(deltas):
-        return any(deltas)
-
-    def check_alloc_deltas(deltas):
-        # At least 1/3rd of 0s
-        if 3 * deltas.count(0) < len(deltas):
-            return True
-        # Nothing else than 1s, 0s and -1s
-        if not set(deltas) <= {1, 0, -1}:
-            return True
-        return False
-
-    leaks = Leaks()
-    for deltas, item_name, checker in [
-        (rc_deltas, 'refs', check_rc_deltas),
-        (alloc_deltas, 'blocks', check_alloc_deltas)
-    ]:
-        if checker(deltas):
-            leaks[item_name] = deltas[nwarmup:]
-    return leaks
-
-
-# The following code is mostly copied from Python 2.7 / 3.5 dash_R_cleanup
-def cleanup(warning_filters, copyreg_dispatch_table, path_importer_cache,
-            zip_directory_cache, abcs):
-    import copyreg
-    import re
-    import warnings
-    import _strptime
-    import linecache
-    import urllib.parse
-    import urllib.request
-    import mimetypes
-    import doctest
-    import struct
-    import filecmp
-    import collections.abc
-    from distutils.dir_util import _path_created
-    from weakref import WeakSet
-
-    # Clear the warnings registry, so they can be displayed again
-    for mod in sys.modules.values():
-        if hasattr(mod, '__warningregistry__'):
-            del mod.__warningregistry__
-
-    # Restore some original values.
-    warnings.filters[:] = warning_filters
-    copyreg.dispatch_table.clear()
-    copyreg.dispatch_table.update(copyreg_dispatch_table)
-    sys.path_importer_cache.clear()
-    sys.path_importer_cache.update(path_importer_cache)
-    try:
-        import zipimport
-    except ImportError:
-        pass  # Run unmodified on platforms without zipimport support
-    else:
-        zipimport._zip_directory_cache.clear()
-        zipimport._zip_directory_cache.update(zip_directory_cache)
-
-    # clear type cache
-    sys._clear_type_cache()
-
-    # Clear ABC registries, restoring previously saved ABC registries.
-    for a in collections.abc.__all__:
-        abc = getattr(collections.abc, a)
-        if not isabstract(abc):
-            continue
-        for obj in abc.__subclasses__() + [abc]:
-            obj._abc_registry = abcs.get(obj, WeakSet()).copy()
-            obj._abc_cache.clear()
-            obj._abc_negative_cache.clear()
-
-    # Flush standard output, so that buffered data is sent to the OS and
-    # associated Python objects are reclaimed.
-    for stream in (sys.stdout, sys.stderr, sys.__stdout__, sys.__stderr__):
-        if stream is not None:
-            stream.flush()
-
-    # Clear assorted module caches.
-    _path_created.clear()
-    re.purge()
-    _strptime._regex_cache.clear()
-    urllib.parse.clear_cache()
-    urllib.request.urlcleanup()
-    linecache.clearcache()
-    mimetypes._default_mime_types()
-    filecmp._cache.clear()
-    struct._clearcache()
-    doctest.master = None
-    try:
-        import ctypes
-    except ImportError:
-        # Don't worry about resetting the cache if ctypes is not supported
-        pass
-    else:
-        ctypes._reset_cache()
-
-    # Collect cyclic trash and read memory statistics immediately after.
-    func1 = sys.getallocatedblocks
-    func2 = sys.gettotalrefcount
-    gc.collect()
-    return func1(), func2()
+    ns = Namespace()
+    ns.quiet = True
+    ns.huntrleaks = (nwarmup, ntracked, "")
+    return refleak.dash_R(ns, "", func)
